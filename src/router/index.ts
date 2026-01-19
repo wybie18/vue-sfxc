6cdcc9bfeaa7b093cf3c624cef8b0823daf74a9b
Wybie <lyrncofyn@gmail.com>
2026-01-19 14:02:43 +0800

    feat: set up router with home route and guest layout

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,18 @@
+import Home from '@/views/Home.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+            meta: {
+                layout: 'guest',
+            },
+        },
+    ],
 })
 
 export default router
